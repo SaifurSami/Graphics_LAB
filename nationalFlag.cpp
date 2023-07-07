43ae322570b90ab096be8f063152c75df2a98fc2
Sami <saifurrahmansami@gmail.com>
2023-07-07 23:34:17 +0600

    Nation Flag Done

---
+++ nationalFlag.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+int main() {
+    int gd = DETECT,gm;
+    initgraph(&gd,&gm,"");
+    //getch();
+    //initwindow(16*50,9*50);
+    //getch();
+    //rectangle
+    setcolor(GREEN);
+    rectangle(40,80,600,400);
+    setfillstyle(SOLID_FILL,GREEN);
+    floodfill(100,100,GREEN);
+
+    //circle
+    setcolor(RED);
+    circle(320,240,100);
+    setfillstyle(SOLID_FILL,RED);
+    floodfill(320,240,RED);
+
+    getch();
+    closegraph();
+    return 0;
+
+}
